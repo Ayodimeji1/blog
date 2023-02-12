cab1bb410fa2aee316e9fcb491af687bba7c52ed
Ayodimeji Kadri <ayodimejia1@gmail.com>
2023-02-12 21:07:49 +0100

    2nd commit

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from. import schemas, models 
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -21,4 +21,38 @@ def create(request: schemas.Blog, db:Session = Depends(get_db)):
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
-    return new_blog
+    return new_blog 
+
+
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db:Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail=f'Blog with id {id} notfound')
+    blog.update(request)
+    db.commit()
+    return 'updated'
+
+
+
+@app.get('/blog')
+def get_all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}')
+def show_blog(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not available')
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail':f'Blog with id {id} not available'}
+    return blog
--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,4 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
-    # is_active = Column(Boolean, default=True)
-
-    # items = relationship("Item", back_populates="owner")
+    
