a23c92e6e9845f4665f9edf5c70b8247a84b65e1
Ayodimeji Kadri <ayodimejia1@gmail.com>
2023-02-15 03:38:01 +0100

    3rd commit

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,9 @@
+from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from. import schemas, models 
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 
 
 app = FastAPI()
@@ -26,7 +28,11 @@ def create(request: schemas.Blog, db:Session = Depends(get_db)):
 
 @app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db:Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail=f'Blog with id {id} notfound')
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
 
@@ -37,18 +43,18 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                              detail=f'Blog with id {id} notfound')
-    blog.update(request)
+    blog.update({'title':request.title, 'body':request.body})
     db.commit()
     return 'updated'
 
 
 
-@app.get('/blog')
+@app.get('/blog', response_model = List[schemas.ShowBlog])
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show_blog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -56,3 +62,14 @@ def show_blog(id, response: Response, db: Session = Depends(get_db)):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'detail':f'Blog with id {id} not available'}
     return blog
+
+
+
+@app.post('/user')
+def create_user(request: schemas.User, db:Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
