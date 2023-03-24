b399749ebf8c2cf483ea0432c4f0ba1176fa4139
Ayodimeji Kadri <ayodimejia1@gmail.com>
2023-03-24 10:40:57 +0000

    9th commit

--- blog/hashing.py
+++ blog/hashing.py
@@ -1,14 +1,16 @@
 from passlib.context import CryptContext
-from passlib.exc import UnknownHashError
 from passlib.hash import bcrypt
+
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class Hash():
-    def bcrypt(password: str) -> str:
-        hashedPassword = pwd_context.hash(password)
-        return hashedPassword
+    def get_password(password: str):
+        return pwd_context.hash(password)
     
-
-    def verify(plain_password: str, hashed_password: str) -> bool:
-        return pwd_context.verify(plain_password,hashed_password) 
+    def verify( plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password) 
+    
+    
+          
--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from blog.hashing import Hash
 from .. import database, schemas, models
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from passlib.hash import bcrypt
 
 
 router = APIRouter(
@@ -10,6 +12,16 @@ router = APIRouter(
     tags=['Authentication'],
 )
 
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# class Hash():
+# def verify(plain_password, hashed_password):
+#     return pwd_context.verify(plain_password,hashed_password)
+
+# def bcrypt(password):
+#     hashedPassword = pwd_context.hash(password)
+#     return hashedPassword
+    
 
 @router.post('/')
 def login(request: schemas.Login, db: Session = Depends(database.get_db)):
@@ -18,4 +30,4 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Invalid username')
     if not Hash.verify(user.password, request.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'incorrect password')
-    return user
+    return user 
